1284747728140281a6ebba0961c58d309ee73b8f
aeross <andrewpratama22@gmail.com>
2023-12-12 21:02:57 +1100

    POST /business and GET /business/[id] done

--- fution-app/src/app/api/business/route.ts
+++ fution-app/src/app/api/business/route.ts
@@ -1,12 +1,24 @@
-import Business from "@/models/business";
+import BusinessModel from "@/models/business";
 import { NextRequest, NextResponse } from "next/server";
 import { APIResponse } from "../typedef";
 
 export async function GET() {
-    const businesses = await Business.get();
+    const businesses = await BusinessModel.readAll();
     return NextResponse.json<APIResponse<unknown>>({
         status: 200,
         message: "success GET /business",
         data: businesses
     });
+}
+
+export async function POST(req: NextRequest) {
+    const input = await req.json();
+    const newBusiness = await BusinessModel.add(input);
+    return NextResponse.json<APIResponse<unknown>>({
+        status: 201,
+        message: "success POST /business",
+        data: newBusiness
+    }, {
+        status: 201
+    })
 }
--- fution-app/src/models/business.ts
+++ fution-app/src/models/business.ts
@@ -1,7 +1,34 @@
+import { Business } from "@prisma/client";
 import prisma from "../../prisma/config";
-
-export default class Business {
-    static async get() {
+/*
+type Business = {
+    id: string;
+    name: string;
+    monthlyRevenue: runtime.Decimal;
+    creditScore: number;
+    description: string;
+    credential: number;
+    tagline: string;
+    userId: string;
+}
+*/
+export default class BusinessModel {
+    static async readAll() {
         return await prisma.business.findMany();
+    }
+
+    static async readById(id: string) {
+        return await prisma.business.findFirst({ where: { id } });
+    }
+
+    static async add(input: {
+        name: string;
+        monthlyRevenue: number;
+        creditScore: number;
+        description: string;
+        tagline: string;
+        userId: string;
+    }) {
+        return await prisma.business.create({ data: input });
     }
 }
