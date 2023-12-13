10ba88d459ad8d5b95e5e5ce1536799e4da3c517
taufiksatriaa <kamikazesatria@gmail.com>
2023-12-13 15:17:34 +0700

    push serverTaufik

--- fution-app/src/app/api/record/[id]/route.ts
+++ fution-app/src/app/api/record/[id]/route.ts
@@ -3,37 +3,51 @@ import errorHandler from "../../errorHandler";
 import RecordModel from "@/models/record";
 import { APIResponse } from "../../typedef";
 
-export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const { id } = params;
-        const record = await RecordModel.readById(id);
-        
-        if (!record) {
-            throw new Error("data not found");
-        }
-        
-        return NextResponse.json<APIResponse<unknown>>(
-            { status: 200, message: "success GET /record/[id]", data: record }
-        );
-    } catch (error) {
-        return errorHandler(error);
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+    const record = await RecordModel.readById(id);
+
+    if (!record) {
+      throw new Error("data not found");
     }
+
+    return NextResponse.json<APIResponse<unknown>>({
+      status: 200,
+      message: "success GET /record/[id]",
+      data: record,
+    });
+  } catch (error) {
+    return errorHandler(error);
+  }
 }
 
-export async function PUT(_req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const { id } = params;
-        // ...
-    } catch (error) {
-        return errorHandler(error);
-    }
+// Put si record
+export async function PUT(
+  _req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+    // ...
+  } catch (error) {
+    return errorHandler(error);
+  }
 }
 
-export async function PATCH(_req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const { id } = params;
-        // ...
-    } catch (error) {
-        return errorHandler(error);
-    }
+// sirecord status
+export async function PATCH(
+  _req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+    const data = await RecordModel.readById(id);
+    console.log(data, "iniii");
+  } catch (error) {
+    return errorHandler(error);
+  }
 }
