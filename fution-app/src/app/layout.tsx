7b1c537b02f07741fd49f3620e0627c5a6a69f9b
aeross <andrewpratama22@gmail.com>
2023-12-17 18:09:32 +1100

    fixed navbar not sticking at the top when scrolling

--- fution-app/src/app/layout.tsx
+++ fution-app/src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav />
+        <div className="sticky top-0 z-20">
+          <Nav />
+        </div>
         <div className="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100">
           {children}
         </div>
